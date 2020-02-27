cbbff9033c17fcddade2905401fe0995be878f55
PaulCollins99 <paul.collins@node-it.com>
2020-02-27 13:46:47 +0000

    add localstorage account and couple of comments

--- files/scripts/script.js
+++ files/scripts/script.js
@@ -2,24 +2,29 @@ function login() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    if (username == "username" && password == "password") {
+
+    // we need our own password database just incase people dont want to use google sign in. Will just use local storage until this is stored
+    if (username == localStorage.getItem("login-username") && password == localStorage.getItem("login-password")) {
         console.log("Access Granted");
+        localStorage.setItem("Username", username);
         window.location.href = 'menu.html';
     } else {
-        console.log("Fuck off");
+        console.log("Access Denied");
     }
 }
 
 function onSignIn(googleUser) {
     let profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('ID: ' + profile.getId()); // Gotta use sommin called an id token here... not sure what that is tbh
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    console.log('Email: ' + profile.getEmail()); 
   }
 
 function boot() {
-    window.loginBtn.addEventListener("click", login)
+    window.loginBtn.addEventListener("click", login);
+    localStorage.setItem("login-username", "admin");
+    localStorage.setItem("login-password", "pass");
 }
 
 window.addEventListener("load", boot);
