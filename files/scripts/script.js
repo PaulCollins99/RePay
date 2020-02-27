c4bd2c137f407eea0631f53709789c847f6a4644
PaulCollins99 <paul.collins@node-it.com>
2020-02-27 13:10:33 +0000

    added menu.html

--- files/scripts/script.js
+++ files/scripts/script.js
@@ -3,7 +3,8 @@ function login() {
     let password = document.getElementById("password").value;
 
     if (username == "username" && password == "password") {
-        console.log("Access Granted")
+        console.log("Access Granted");
+        window.location.href = 'menu.html';
     } else {
         console.log("Fuck off");
     }
