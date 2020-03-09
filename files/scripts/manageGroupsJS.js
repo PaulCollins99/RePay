38dea4feccd708632707df23453a4c167c33ce4c
PaulCollins99 <paul.collins@node-it.com>
2020-03-09 12:08:04 +0000

    changing manage groups screen

--- files/scripts/manageGroupsJS.js
+++ files/scripts/manageGroupsJS.js
@@ -1,15 +1,32 @@
 //function to add new list item into the group list
 
+function addTextBox () {
+    const element = document.getElementById("manageGroupsDiv");
+
+    const newElement = document.createElement("input");
+    newElement.placeholder = "Enter Name";
+    
+    const newButton = document.createElement("button");
+    newButton.textContent = "Create"
+
+    const oldAddButton = document.getElementById("addNewList")
+    oldAddButton.style.display = "none";
+
+    element.appendChild(newElement);
+    element.appendChild(newButton);
+
+}
+
 function addLi () {
     const element = document.getElementById("listOfGroups");
     const newElement = document.createElement("li");
-    newElement.textContent = "Hi";
+    newElement.textContent = "Group name here";
     element.appendChild(newElement);
 }
 
 
 function boot () {
-    window.addEventListener("click", addLi)
+    window.addNewList.addEventListener("click", addLi)
 }
 
 window.addEventListener("load", boot);
