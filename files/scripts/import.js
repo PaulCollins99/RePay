97e4ec7355d8be7702c5167d7cfadd92df37bf05
PaulCollins99 <paul.collins@node-it.com>
2020-03-10 11:26:35 +0000

    added importing files

---
+++ files/scripts/import.js
@@ -0,0 +1,21 @@
+function importFile() {
+    const element = document.getElementById('mainTextArea');
+    const fileToImport = document.getElementById('fileToImport');
+    const file = fileToImport.files[0];
+    const fileReader = new FileReader();
+  
+    // eslint-disable-next-line func-names
+    fileReader.addEventListener('load', function () {
+      element.value = this.result;
+    });
+    fileReader.readAsText(file);
+  }
+
+function boot () {
+
+    window.btnImport.addEventListener("click", importFile);
+
+
+}
+
+window.addEventListener("load", boot);
