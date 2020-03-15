397f69327f1af9becd61769c3c26d64a86090d13
PaulCollins99 <paul.collins@node-it.com>
2020-03-15 15:34:02 +0000

    more fix

--- files/scripts/groupDetails.js
+++ files/scripts/groupDetails.js
@@ -1,18 +1,31 @@
 function addUser(user) {
     let groupToEdit = localStorage.getItem("groupToLoad");
     let groupArray = JSON.parse(localStorage.getItem("groupList"));
-
-    for (let x = 0; x < groupArray.length; x++) {
-        let array = JSON.parse(groupArray[x]);
-        if (array[0] == groupToEdit) {
-            let users = JSON.parse(array[1]);
-            users.push(user);
-            array[1] = JSON.stringify(users);
-            let element = JSON.stringify(array);
-            groupArray[x] = element;
-            localStorage.setItem("groupList", JSON.stringify(groupArray));
+    let potentialUsers = JSON.parse(localStorage.getItem("userList"));
+    potentialUsers.forEach(e => {
+        if (e == user) {
+            for (let x = 0; x < groupArray.length; x++) {
+                let array = JSON.parse(groupArray[x]);
+                if (array[0] == groupToEdit) {
+                    let users = JSON.parse(array[1]);
+                    let check = users.filter (e => e = user)
+                    if (check == "") {
+                        users.push(user);
+                        array[1] = JSON.stringify(users);
+                        let element = JSON.stringify(array);
+                        groupArray[x] = element;
+                        localStorage.setItem("groupList", JSON.stringify(groupArray));
+                    }
+                    else {
+                        alert("User is already a member of the group");
+                    }
+                }
+            }
+        } else {
+            alert("User does not exist");
         }
-    }
+    });
+
 }
 
 function changeGroupName(groupName) {
@@ -32,16 +45,16 @@ function changeGroupName(groupName) {
     }
 }
 
-function deleteGroup () {
+function deleteGroup() {
     let groupToDelete = localStorage.getItem("groupToLoad");
     let groupArray = JSON.parse(localStorage.getItem("groupList"));
     console.log(groupArray);
-    
+
     for (let x = 0; x < groupArray.length; x++) {
         let array = JSON.parse(groupArray[x]);
         console.log(array);
         if (array[0] == groupToDelete) {
-            groupArray.splice(x,1);
+            groupArray.splice(x, 1);
             localStorage.setItem("groupList", JSON.stringify(groupArray));
         }
     }
