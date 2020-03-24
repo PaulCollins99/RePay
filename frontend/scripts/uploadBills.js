d132f149d2c69cef9278b4c81bf9763b51c55f5c
PaulCollins99 <paul.collins@node-it.com>
2020-03-24 13:33:02 +0000

    started working on upload bill

--- frontend/scripts/uploadBills.js
+++ frontend/scripts/uploadBills.js
@@ -1,22 +1,47 @@
 //doesnt really work as intended
 
 function importFile() {
-    const element = document.getElementById('mainTextArea');
-    const fileToImport = document.getElementById('fileToImport');
-    const file = fileToImport.files[0];
-    const fileReader = new FileReader();
-  
-    // eslint-disable-next-line func-names
-    fileReader.addEventListener('load', function () {
-      element.value = this.result;
-    });
-    fileReader.readAsText(file);
-  }
+  const element = document.getElementById('mainTextArea');
+  const fileToImport = document.getElementById('fileToImport');
+  const file = fileToImport.files[0];
+  const fileReader = new FileReader();
 
-function boot () {
+  fileReader.addEventListener('load', function () {
+    element.value = this.result;
+  });
+  fileReader.readAsText(file);
+}
 
-    window.btnImport.addEventListener("click", importFile);
+function addSelectedNames(e) {
+  console.log(e.target.textContent);
 
+}
+
+function selectAll() {
+  let userArray = [];
+  let groupToLoad = localStorage.getItem("groupToLoad");
+  let groupArray = JSON.parse(localStorage.getItem("groupList"));
+  groupArray.forEach(element => {
+    let array = JSON.parse(element);
+    if (array[0] == groupToLoad) {
+      let users = JSON.parse(array[1]);
+      users.forEach(element => userArray.push(element))
+    }
+  });
+  console.log(userArray);
+}
+
+function importBill(total, arrayOfGroupMembers) {
+  console.log(total, arrayOfGroupMembers);
+
+}
+
+function boot() {
+
+  window.btnImport.addEventListener("click", importFile);
+  window.test1.addEventListener("click", addSelectedNames);
+  window.test2.addEventListener("click", addSelectedNames);
+  window.selectAllBtn.addEventListener("click", selectAll);
 
 }
 
